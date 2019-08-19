cdf0fe5d7f3e2fb043440ba50b56152f63fccc85
unknown <andris.strozs@gmail.com>
2019-08-19 19:35:33 +0300

    added loops exercises

---
+++ loops/Exercise4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        //TODO: print all vowels using for and foreach
+        static void Main(string[] args)
+        {
+            char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+            
+            //todo - use for
+            for (var i = 0; i < vowels.Length; i++) {
+                Console.WriteLine(vowels[i]);
+            }
+            
+            Console.WriteLine();
+            
+            //todo - use foreach
+            foreach (char vowel in vowels) {
+                Console.WriteLine(vowel);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
