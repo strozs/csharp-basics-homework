d104727cfa1f85ed7d81d0c25952e41913b84120
unknown <andris.strozs@gmail.com>
2019-08-20 11:39:49 +0300

    added arrays

---
+++ arrays/Arrays/Exercise1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        //TODO: Write a C# program to sort a numeric array and a string array.
+        private static void Main(string[] args)
+        {
+            int[] myArray1 = {
+                1789, 2035, 1899, 1456, 2013,
+                1458, 2458, 1254, 1472, 2365,
+                1456, 2165, 1457, 2456
+            };
+
+            string[] myArray2 = {
+                "Java",
+                "Python",
+                "PHP",
+                "C#",
+                "C Programming",
+                "C++"
+            };
+
+
+            //fixme
+            //Console.WriteLine("Original numeric array : " + myArray1);
+            foreach (var item in myArray1)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            //Sort array
+            //Console.WriteLine("Sorted numeric array : " + Array.Sort(myArray1));
+            Array.Sort(myArray1);
+            foreach (var item in myArray1)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            //Console.WriteLine("Original string array : " + );
+            foreach (var item in myArray2)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            //Sort array
+            //Console.WriteLine("Sorted string array : " + );
+            Array.Sort(myArray2);
+            foreach (var item in myArray2)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ arrays/Arrays/Exercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise2
+{
+    class Program
+    {
+        //TODO: Write a C# program to sum values of an array.
+        private static void Main(string[] args)
+        {
+            int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            var sum = myArray.Sum();
+
+
+            //fixme
+            /*for (int i = 0; i <= myArray.Length; i++)
+            {
+                Console.WriteLine(sum);
+                
+            }*/
+
+
+
+            Console.WriteLine("The sum is " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
