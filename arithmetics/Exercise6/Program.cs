d9155a4f1a2378db4cc7841fb15c9a07d2b0f1cf
strozs <andris.strozs@gmail.com>
2019-08-22 10:19:33 +0300

    Checked and finished some exercises

--- arithmetics/Exercise6/Program.cs
+++ arithmetics/Exercise6/Program.cs
@@ -13,14 +13,29 @@ namespace Exercise6
             Console.WriteLine("Program CozaLozaWoza");
             for (int i = 1; i <= 110; i++)
             {
-                Console.Write(i + " ");
-                if (i % 11 == 0)
+                if (i % 11 == 0 + 1)
                 {
                     Console.WriteLine();
                 }
+                if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+                {
+                    Console.Write("CozaLozaWoza ");
+                }
                 else if (i % 3 == 0)
                 {
-                    Console.Write("Coza");
+                    Console.Write("Coza ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Loza ");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write("Woza ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
                 }
             }
             Console.ReadKey();
