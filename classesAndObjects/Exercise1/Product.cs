a3a3d14cab4052067667876894f396feea03379e
strozs <andris.strozs@gmail.com>
2019-08-22 15:04:49 +0300

    Added Exercise 1

---
+++ classesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Product
+    {
+        private string _name;
+        private double _priceAtStart;
+        private int _amountAtStart;
+
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            this._name = name;
+            this._priceAtStart = priceAtStart;
+            this._amountAtStart = amountAtStart;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name}, price {_priceAtStart}, amount {_amountAtStart}");
+        }
+
+        public void AddAmount()
+        {
+            _amountAtStart++;
+        }
+    }
+}
---
+++ classesAndObjects/Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mouse = new Product("Mouse", 79.99, 12);
+            var phone = new Product("iPhone 5s", 499.99, 7);
+            var speakers = new Product("Epson EB-U05", 299.99, 23);
+
+            mouse.PrintProduct();
+            phone.PrintProduct();
+            speakers.PrintProduct();
+
+            mouse.AddAmount();
+
+            mouse.PrintProduct();
+
+            Console.ReadKey();
+        }
+    }
+}
