600168c28d5c17dbbab91c85eae3ce8cfbfeb3ea
strozs <andris.strozs@gmail.com>
2019-08-27 19:21:15 +0300

    Finished excersise 5

--- collections/Exercise5/Program.cs
+++ collections/Exercise5/Program.cs
@@ -11,19 +11,39 @@ namespace Exercise5
         static void Main(string[] args)
         {
             Console.Write("Enter a Number: ");
-            string input = Console.ReadLine();
-            char[] charArr = input.ToCharArray();
+            string input = Console.ReadLine(); // Gets user input
+            char[] charArr = input.ToCharArray(); //Converts input to char array
+            List<char> lst = charArr.OfType<char>().ToList(); // converts char array to a list of chars
             int sum = 0;
-            
 
-            foreach (char ch in charArr)
+            do
             {
-                int result = int.Parse(ch.ToString());
-                result *= result;
-                Console.WriteLine(result);
-                sum += result;
+                sum = 0;
+                foreach (char ch in lst) 
+                {
+                    int number = int.Parse(ch.ToString()); // Converts char to string and then to int
+                    number *= number;
+                    Console.WriteLine(number);
+                    sum += number;
+                }
+                Console.WriteLine("Sum is " + sum);
+                lst.Clear(); // Clears original list
+                string lstToString = Convert.ToString(sum); // Converts sum to string
+                char[] newCharArr = lstToString.ToCharArray(); // Converts string to char array
+                List<char> newLst = newCharArr.OfType<char>().ToList(); // converts char array to a list of chars
+                lst.AddRange(newLst); // Adds the new list values to old list
+            } while (lst.Count != 1);
+
+            Console.WriteLine();
+
+            if (sum == 1)
+            {
+                Console.WriteLine($"{input} is a HAPPY NUMBER!");
             }
-            Console.WriteLine(sum);
+            else
+            {
+                Console.WriteLine($"{input} is NOT a HAPPY NUMBER!");
+            }
 
             Console.ReadKey();
         }
