45af2f5d3dfbf870c330387fa4805d9a5b7b387a
strozs <andris.strozs@gmail.com>
2019-08-28 13:34:41 +0300

    Started exercises 7 and 9

--- collections/Exercise7/Program.cs
+++ collections/Exercise7/Program.cs
@@ -12,35 +12,30 @@ namespace Exercise7
         private const string Path = "D:/Codelex/csharp-basics-homework/collections/Exercise7/flights.txt";
         static void Main(string[] args)
         {
-            var readText = File.ReadAllLines(Path);
-            foreach (var s in readText)
+            List<string> flights = File.ReadAllLines(Path).ToList();
+            foreach (var s in flights)
             {
                 Console.WriteLine(s);
             }
 
-            var flights = new Dictionary<string, string>();
-            flights.Add("San Jose", "San Francisco");
-            flights.Add("San Jose", "Anchorage"); //Shows error, becouse you cant add to of the same kays to a dictionary
-            flights.Add("New York", "Anchorage");
-            flights.Add("New York", "San Jose");
-            flights.Add("New York", "San Francisco");
-            flights.Add("New York", "Honolulu");
-            flights.Add("Anchorage", "New York");
-            flights.Add("Anchorage", "San Jose");
-            flights.Add("Honolulu", "New York");
-            flights.Add("Honolulu", "San Francisco");
-            flights.Add("Denver", "San Jose");
-            flights.Add("San Francisco", "New York");
-            flights.Add("San Francisco", "Honolulu");
-            flights.Add("San Francisco", "Denver");
+            var listOfCities = new HashSet<string>();
+            for (int i = 0; i < flights.Count; i++)
+            {
+                listOfCities.Add(flights[i].Substring(0, flights[i].IndexOf("->")));
+            }
 
-            string allFlights = string.Join(";", flights.Select(x => x.Key + "=" + x.Value).ToArray());
-            Console.WriteLine(allFlights);
+            Console.WriteLine("List of cities: ");
+            foreach (var item in listOfCities)
+            {
+                Console.WriteLine(item);
+            }
+
+            
 
 
-            Console.WriteLine("What would you like to do:");
+           /* Console.WriteLine("What would you like to do:");
             Console.WriteLine("To display list of the cities press 1");
-            Console.WriteLine("To exit program press 0");
+            Console.WriteLine("To exit program press 0");*/
 
 
             Console.ReadKey();
---
+++ collections/Exercise9/PhoneEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class PhoneEntry
+    {
+        public string name;
+        public string number;
+    }
+}
