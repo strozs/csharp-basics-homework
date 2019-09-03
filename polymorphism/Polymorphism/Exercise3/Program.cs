3b9c336c491ff3554bfbd5c9a21a2056f7953947
strozs <andris.strozs@gmail.com>
2019-09-03 17:32:06 +0300

    started exercise 3

---
+++ polymorphism/Polymorphism/Exercise3/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * - Create an instant of class Student and an instant of class Employee with proper values for the attributes.
+             * - Display the content of each class using Display() method.
+             */
+        }
+    }
+}
